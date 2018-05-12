05058763d623fb829a5b8aa618299987c40cfe7f
Michel Mattos <mattos.michel@gmail.com>
2018-05-13 03:15:12 +1200

    A better way to start server using babel-node

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
+import path from 'path'
 import console from 'console'
 import express from 'express'
 import routes from './routes'
 
 const app = express()
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(routes)
 
 app.listen(3000, () => {
